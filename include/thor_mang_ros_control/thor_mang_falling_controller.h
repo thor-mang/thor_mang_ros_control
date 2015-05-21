2a0205106a37cfcba0293fd99d3059e7207f6e58
Christian Rose <christian.j.rose@gmx.de>
2015-05-21 13:07:58 -0400

    basic functionallty for fall detection. When falling turn on lights, wait 500ms and then turn off torque

---
+++ include/thor_mang_ros_control/thor_mang_falling_controller.h
@@ -0,0 +1,85 @@
+#ifndef THOR_MANG_FALLING_CONTROLLER_H
+#define THOR_MANG_FALLING_CONTROLLER_H
+
+#include "ros/ros.h"
+
+// ros control
+#include <controller_interface/controller.h>
+#include <pluginlib/class_list_macros.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <hardware_interface/imu_sensor_interface.h>
+#include <motion/motionmodule.h>
+
+// THOR-OP includes
+#include <framework/Thor.h>
+
+//Mode switcher action client
+#include <actionlib/client/simple_action_client.h>
+#include <vigir_humanoid_control_msgs/ChangeControlModeAction.h>
+
+namespace Thor
+{
+
+typedef actionlib::SimpleActionClient<vigir_humanoid_control_msgs::ChangeControlModeAction> ChangeControlModeActionClient;
+
+typedef enum {
+    Disabled,
+    Ready,
+    FallPose,
+    TorqueOff
+}State;
+
+class ThorMangFallingController:
+        public controller_interface::Controller<hardware_interface::ImuSensorInterface>
+        , public MotionModule
+{
+public:
+    ThorMangFallingController();
+
+    // ros control
+    bool init(hardware_interface::ImuSensorInterface* hw, ros::NodeHandle& nh);
+
+    void update(const ros::Time& time, const ros::Duration& period);
+
+    void starting(const ros::Time& time);
+    void stopping(const ros::Time& time);
+
+    // THOR-OP framework
+    void Initialize();
+    void Process();
+
+protected:
+    bool checkFalling();
+    void sendInfoToControlModeSwitcher();
+    void goIntoFallPose();
+    bool checkTorqueOff();
+    void disableTorque();
+
+private:
+    double rollThresholdPositive;
+    double rollThresholdNegative;
+    double pitchThresholdPositive;
+    double pitchThresholdNegative;
+
+    double fallPoseTime;
+
+    std::vector<int> jointIds;
+    std::vector<double> jointValues;
+
+    State falling_state;
+
+    std::string control_mode_switch_name;
+    ChangeControlModeActionClient *action_client;
+
+    hardware_interface::ImuSensorHandle imu_sensor_handle;
+
+    ros::WallTime fallPoseDoneTime;
+
+    void modeSwitchDoneCallback(const actionlib::SimpleClientGoalState& state,  const vigir_humanoid_control_msgs::ChangeControlModeResultConstPtr& result);
+    void modeSwitchActiveCallback();
+    void modeSwitchFeedbackCallback(const vigir_humanoid_control_msgs::ChangeControlModeFeedbackConstPtr &feedback);
+
+};
+
+}
+#endif // THOR_MANG_FALLING_CONTROLLER_H
---
+++ src/thor_mang_falling_controller.cpp
@@ -0,0 +1,163 @@
+#include <thor_mang_ros_control/thor_mang_falling_controller.h>
+
+#include "tf/tf.h"
+#include "motion/motionmanager.h"
+
+namespace Thor
+{
+
+ThorMangFallingController::ThorMangFallingController() : falling_state(Disabled)
+{
+
+}
+
+
+bool ThorMangFallingController::init(hardware_interface::ImuSensorInterface *hw, ros::NodeHandle& nh)
+{
+    nh.param("rollThresholdPositive", rollThresholdPositive, 0.245);
+    nh.param("rollThresholdNegative", rollThresholdNegative, -0.325);
+    nh.param("pitchThresholdPositive", pitchThresholdPositive, 0.3);
+    nh.param("pitchThresholdNegative", pitchThresholdNegative, -0.15);
+    nh.param("fallPoseTime", fallPoseTime, 0.500);
+
+    nh.getParam("jointIds", jointIds);
+    nh.getParam("jointValues", jointValues);
+
+    nh.param<std::string>("control_mode_switch_name", control_mode_switch_name, "/mode_controllers/control_mode_controller/change_control_mode");
+
+    action_client = new ChangeControlModeActionClient(control_mode_switch_name, true);
+
+    imu_sensor_handle = hw->getHandle("pelvis_imu");
+
+    falling_state = Disabled;
+
+    return true;
+}
+
+void ThorMangFallingController::update(const ros::Time& time, const ros::Duration& period)
+{
+
+}
+
+void ThorMangFallingController::starting(const ros::Time& time)
+{
+    MotionManager::GetInstance()->AddModule(this);
+
+    falling_state = Ready;
+
+}
+
+void ThorMangFallingController::stopping(const ros::Time& time)
+{
+    Thor::MotionManager::GetInstance()->RemoveModule(this);
+    falling_state = Disabled;
+}
+
+void ThorMangFallingController::Initialize()
+{
+}
+
+void ThorMangFallingController::Process()
+{
+    switch(falling_state){
+    case Disabled:
+        return;
+    case Ready:
+        if(checkFalling()){
+            goIntoFallPose();
+            falling_state = FallPose;
+        }
+        break;
+    case FallPose:
+        if(checkTorqueOff()){
+            disableTorque();
+            falling_state = TorqueOff;
+        }
+        break;
+    case TorqueOff:
+        return;
+    default:
+        ROS_ERROR("Unknown state");
+    }
+
+}
+
+bool ThorMangFallingController::checkFalling()
+{
+    const double* imu_orientation = imu_sensor_handle.getOrientation();
+
+    double roll = 0, pitch = 0, yaw = 0;
+
+    tf::Quaternion orientation(imu_orientation[0] , imu_orientation[1], imu_orientation[2], imu_orientation[3] );
+
+    tf::Matrix3x3(orientation).getRPY(roll, pitch, yaw);
+
+    if (roll > rollThresholdPositive || roll < rollThresholdNegative
+            || pitch < pitchThresholdNegative || pitch > pitchThresholdPositive){
+        return true;
+    }
+    return false;
+}
+
+void ThorMangFallingController::goIntoFallPose(){
+    ROS_INFO("Going into FALL!");
+
+    fallPoseDoneTime = (ros::WallTime::now() + ros::WallDuration(fallPoseTime));
+    MotionManager::GetInstance()->EnableLights(true);
+
+    //TODO impl
+    //claimJoints
+
+    //set Joint Values
+}
+
+bool ThorMangFallingController::checkTorqueOff(){
+    ros::WallTime current = ros::WallTime::now();
+    if( current > fallPoseDoneTime ){
+        return true;
+    }
+}
+
+void ThorMangFallingController::disableTorque(){
+    ROS_INFO("disable torque!");
+    MotionManager::GetInstance()->EnableLights(false);
+    for (unsigned int joint_index = 0; joint_index < m_RobotInfo.size(); joint_index++)
+    {
+      MotionManager::GetInstance()->SetTorqueOn(m_RobotInfo[joint_index], false);
+    }
+}
+
+
+void ThorMangFallingController::sendInfoToControlModeSwitcher(){
+    if (action_client->isServerConnected()){
+        vigir_humanoid_control_msgs::ChangeControlModeGoal goal;
+
+        goal.mode_request="falling";
+
+        action_client->sendGoal(goal, boost::bind(&ThorMangFallingController::modeSwitchDoneCallback, this, _1, _2), boost::bind(&ThorMangFallingController::modeSwitchActiveCallback, this), boost::bind(&ThorMangFallingController::modeSwitchFeedbackCallback, this, _1));
+        action_client->waitForResult(ros::Duration(5));
+
+    }else{
+        ROS_ERROR("Unable to calll control mode switcher.");
+    }
+}
+
+
+void ThorMangFallingController::modeSwitchDoneCallback(const actionlib::SimpleClientGoalState& state,  const vigir_humanoid_control_msgs::ChangeControlModeResultConstPtr& result){
+    if(state == actionlib::SimpleClientGoalState::SUCCEEDED){
+        //Good
+    }else{
+        //Need to try again
+    }
+}
+
+void ThorMangFallingController::modeSwitchActiveCallback(){
+
+}
+
+void ThorMangFallingController::modeSwitchFeedbackCallback(const vigir_humanoid_control_msgs::ChangeControlModeFeedbackConstPtr &feedback){
+
+}
+}
+
+PLUGINLIB_EXPORT_CLASS(Thor::ThorMangFallingController, controller_interface::ControllerBase)
