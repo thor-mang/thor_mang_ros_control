cb5352a7d2e7b6d7283b18d59b137fdf7bd29428
Marcell Missura <missura@ais.uni-bonn.de>
2015-06-03 10:24:35 +0200

    fall detection test

--- src/thor_mang_falling_controller.cpp
+++ src/thor_mang_falling_controller.cpp
@@ -93,7 +93,11 @@ void ThorMangFallingController::update(const ros::Time& time, const ros::Duratio
             if (detectAndDecide())
             {
                 MotionManager::GetInstance()->EnableLights(true);
-                fallState = Falling;
+                //fallState = Falling;
+            }
+            else
+            {
+                MotionManager::GetInstance()->EnableLights(false);
             }
             break;
         case Falling:
